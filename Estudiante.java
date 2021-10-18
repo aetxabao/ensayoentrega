19355575a2eba35c85855fb85b28437a0a1f96b5
evirunurm <teakid21@gmail.com>
2021-10-18 13:22:03 +0200

    change author

--- Estudiante.java
+++ Estudiante.java
@@ -1,44 +1,44 @@
 
 /**
- *  Descripción de un estudiante
- * 
- * @author - Montse 
- *  
+ *  Descripciï¿½n de un estudiante
+ *
+ * @author - Evelin Virunurm 
+ *
  */
 public class Estudiante
 {
     private String nombre;
     private int nota;
-     
+
 
     /**
-     * Constructor  
+     * Constructor
      */
     public Estudiante(String queNombre, int queNota)    {
         nombre = queNombre;
         nota = queNota;
-         
+
     }
 
     /**
-     * 
+     *
      */
     public String getNombre() {
         return nombre;
-         
+
     }
-    
-    
+
+
      /**
-     * 
+     *
      */
     public int getNota() {
         return nota;
-         
+
     }
-    
+
     /**
-     * 
+     *
      */
     public void printEstudiante() {
         System.out.println("Nombre: " + nombre +
